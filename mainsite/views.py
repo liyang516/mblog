25a3ed8859f441221533105bb6f2b6fcc46b492c
liyang <505984452@qq.com>
2018-01-29 04:17:02 -0800

    完成Page58内容

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from django.template.loader import get_template
+from datetime import datetime
 
 # Create your views here.
+
+# def homepage(request):
+# 	posts = Post.objects.all()
+# 	post_lists = []
+# 	for count,post in enumerate(posts):
+# 		post_lists.append("No.{}:".format(str(count)) + str(post) + "<br>")
+# 		post_lists.append("<small>" + str(post.body.encode('utf-8')) + "</small><br><br>")
+# 	return HttpResponse(post_lists)
+
+def homepage(request):
+	template = get_template('index.html')
+	posts = Post.objects.all()
+	now = datetime.now()
+	html = template.render(locals())
+	return HttpResponse(html)
--- mblog/settings.py
+++ mblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hiuowb7#_lrv2ri14&(t*%%o#q!pzzd=_hsdq9_fo8oawibn6p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.163.137']
+ALLOWED_HOSTS = ['192.168.163.137','127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainsite',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'mblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
