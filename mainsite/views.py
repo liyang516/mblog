a557e721cf273fb643fa2ac330853a6901d94c78
liyang <505984452@qq.com>
2018-01-29 05:51:23 -0800

    完成Page65 2.3内容

--- mainsite/views.py
+++ mainsite/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Post
 from django.template.loader import get_template
 from datetime import datetime
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -20,3 +21,13 @@ def homepage(request):
 	now = datetime.now()
 	html = template.render(locals())
 	return HttpResponse(html)
+
+def showpost(request,slug):
+	template = get_template('post.html')
+	try:
+		post = Post.objects.get(slug=slug)
+		if post != None:
+			html = template.render(locals())
+			return HttpResponse(html)
+	except:
+		return redirect('/')
